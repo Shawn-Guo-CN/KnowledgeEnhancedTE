93573b390be89d2397d3f18963a65e659f5abbaa
Shawn <shawnguo.cn@gmail.com>
2017-03-09 10:43:17 +0800

    Recursive NN is now a vanilla recursive nn model.

--- tree_models.py
+++ tree_models.py
@@ -6,9 +6,11 @@ from torch.autograd import Variable
 class RecursiveNN(nn.Module):
     def __init__(self, word_embedding):
         super(RecursiveNN, self).__init__()
+        self.word_dim = word_embedding.embeddings.size(1)
         self.embedding = nn.Embedding(word_embedding.embeddings.size(0),
-                                      word_embedding.embeddings.size(1))
+                                      self.word_dim)
         self.embedding.weight = nn.Parameter(word_embedding.embeddings)
+        self.linear = nn.Linear(2 * self.word_dim, self.word_dim)
 
     def forward(self, node):
         if not node.val is None:
@@ -16,6 +18,6 @@ class RecursiveNN(nn.Module):
             return node.calculate_result
         else:
             assert len(node.children) == 2
-            node.calculate_result = node.children[0].calculate_result + \
-                                    node.children[1].calculate_result
+            node.calculate_result = self.linear(torch.cat((node.children[0].calculate_result,
+                                                          node.children[1].calculate_result), 1))
             return node.calculate_result
